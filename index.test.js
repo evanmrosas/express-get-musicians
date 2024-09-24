e349793298f2c745bdd851d35c01d61787a49e67
evanmrosas <147665124+evanmrosas@users.noreply.github.com>
2024-09-24 14:16:44 -0500

    finished adding endpoint and testing

--- index.test.js
+++ index.test.js
@@ -9,14 +9,19 @@ const { Musician } = require('./models/index')
 const app = require('./src/app');
 const {seedMusician} = require("./seedData");
 
+beforeAll(() => {
+    execSync('npm install');
+    execSync('npm run seed');
+});
+
+afterAll(async () => {
+    await db.close();
+});
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
-
-
-
-
-    
+    it('should return a 200 status code and a list of musicians', async () => {
+        const response = await request(app).get('/musicians');
+        expect(response.statusCode).toBe(200);
+})
 })

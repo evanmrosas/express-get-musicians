b285abbe100534f76f54e138384d94f21f64a5dc
evanmrosas <evanmrosas@gmail.com>
2024-09-25 12:21:19 -0500

    added the rest of the endpoints for CRUD work

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const { Musician } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //TODO: Create a GET /musicians route to return all musicians 
 app.get("/musicians", async (req, res) => {
@@ -15,9 +17,21 @@ app.get("/musicians/:id", async (req, res) => {
     const musician = await Musician.findByPk(id)
     res.json(musician)
 })
-
-
-
+app.post("/musicians", async (req, res) => {
+    const newMusician = await Musician.create(req.body)
+    res.json(newMusician)
+})
+app.put("/musicians/:id", async (req, res) => {
+    const id = req.params.id
+    const updateMusician = await Musician.findByPk(id)
+    await updateMusician.update(req.body)
+    res.json(updateMusician)
+})
+app.delete("/musicians/:id", async (req, res) => {
+    const id = req.params.id
+    const deleteMusician = await Musician.findByPk(id)
+    await deleteMusician.destroy()
+})
 
 
 

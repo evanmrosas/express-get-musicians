69a959ac125f43baa0441290ba7f8fd6a4e3be7f
evanmrosas <evanmrosas@gmail.com>
2024-09-25 15:46:34 -0500

    added express routes

--- src/app.js
+++ src/app.js
@@ -1,38 +1,11 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
-
-const port = 3000;
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
 //TODO: Create a GET /musicians route to return all musicians 
-app.get("/musicians", async (req, res) => {
-    const musicians = await Musician.findAll()
-    res.json(musicians)
-})
-app.get("/musicians/:id", async (req, res) => {
-    const id = req.params.id
-    const musician = await Musician.findByPk(id)
-    res.json(musician)
-})
-app.post("/musicians", async (req, res) => {
-    const newMusician = await Musician.create(req.body)
-    res.json(newMusician)
-})
-app.put("/musicians/:id", async (req, res) => {
-    const id = req.params.id
-    const updateMusician = await Musician.findByPk(id)
-    await updateMusician.update(req.body)
-    res.json(updateMusician)
-})
-app.delete("/musicians/:id", async (req, res) => {
-    const id = req.params.id
-    const deleteMusician = await Musician.findByPk(id)
-    await deleteMusician.destroy()
-})
+const musicianRoute = require("./routes/musicians")
 
+app.use("/musicians", musicianRoute)
 
+app.use("/musicians/:id", musicianRoute)
 
 module.exports = app;
---
+++ src/routes/musicians.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const router = express.Router()
+const Musician = require("../../models/index")
+const db = require("../../db/connection");
+
+router.use(express.json())
+router.use(express.urlencoded({ extended: true }))
+// GET all
+router.get("/", async (req, res) => {
+    const musicians = await Musician.findAll()
+    res.json(musicians)
+})
+// GET one
+router.get("/:id", async(req, res) => {
+    const id = req.params.id
+    const musician = await Musician.findByPk(id)
+    res.json(musician)
+})
+// CREATE one
+router.post("/", async (req, res) => {
+    const newMusician = await Musician.create(req.body)
+    res.json(newMusician)
+})
+// UPDATE one
+router.put("/:id", async (req, res) => {
+    const id = req.params.id
+    const updateMusician = await Musician.findByPk(id)
+    await updateMusician.update(req.body)
+    res.json(updateMusician)
+})
+// DELETE one
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id
+    const deleteMusician = await Musician.findByPk(id)
+    await deleteMusician.destroy()
+})
+module.exports = router;
